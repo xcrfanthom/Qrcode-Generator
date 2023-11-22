7347e3d8436c573dd32df0d8125b42dac3f328d2
Bartu_Akarsu <akarsubartu@gmail.com>
2023-11-22 19:00:05 +0300

    commit

--- qrcode.py
+++ qrcode.py
@@ -1,2 +1,10 @@
-print("hello")
-print("Hi")
+import segno
+import random
+
+i = input("Enter a link or a phrase that you want it to be qrcoded: ")
+n = input("Enter a size for the qrcode: ")
+qrcode = segno.make_qr(i)
+qrcode.save(
+    f"{random.random()}.png",
+    scale = n    
+)
